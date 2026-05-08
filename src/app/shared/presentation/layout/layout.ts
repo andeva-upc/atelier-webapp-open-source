89cd730e2a93cfba87e5fd60a3253fee7125565d
shouydev <shouyestefanero@gmail.com>
2026-05-07 22:06:27 -0500

    feat(shared): implement layout structure with sidebar and main content.

---
+++ src/app/shared/presentation/layout/layout.ts
@@ -0,0 +1,18 @@
+import { Component, signal } from '@angular/core';
+import { LeftSidebar } from '../left-sidebar/left-sidebar';
+import { Main } from '../main/main';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  imports: [LeftSidebar, Main, RouterOutlet],
+  templateUrl: './layout.html',
+  styleUrl: './layout.css',
+})
+export class Layout {
+  isLeftSidebarCollapsed = signal<boolean>(false);
+
+  changeIsLeftSidebarCollapsed(isLeftSidebarCollapsed: boolean) {
+    this.isLeftSidebarCollapsed.set(isLeftSidebarCollapsed);
+  }
+}
