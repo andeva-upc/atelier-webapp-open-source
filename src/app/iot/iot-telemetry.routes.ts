5f36a84b2a04734050aa673e261ff4fd81b602c3
danieltyuyu <di0014617@gmail.com>
2026-06-19 16:00:47 -0500

    feat(iot): configure route for dedicated obd2 devices view.

--- src/app/iot/iot-telemetry.routes.ts
+++ src/app/iot/iot-telemetry.routes.ts
@@ -2,5 +2,9 @@ import { Routes } from '@angular/router';
 import { TelemetryDashboardComponent } from './presentation/views/telemetry-dashboard/telemetry-dashboard';
 
 export const TELEMETRY_ROUTES: Routes = [
-  { path: '', component: TelemetryDashboardComponent }
+  { path: '', component: TelemetryDashboardComponent },
+  {
+    path: 'odb2_devices',
+    loadComponent: () => import('./presentation/views/obd2-devices-list/obd2-devices-list').then(m => m.Obd2DevicesListComponent)
+  }
 ];
