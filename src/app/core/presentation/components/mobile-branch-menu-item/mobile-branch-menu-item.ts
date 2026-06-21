8514a9bc84763653e7b52259da9165f57201b113
shouydev <shouyestefanero@gmail.com>
2026-06-21 03:26:34 -0500

    fix(mobile-branch-menu-item): remove unused view child import for cleaner code.

--- src/app/core/presentation/components/mobile-branch-menu-item/mobile-branch-menu-item.ts
+++ src/app/core/presentation/components/mobile-branch-menu-item/mobile-branch-menu-item.ts
@@ -1,4 +1,4 @@
-import { Component, inject, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, inject, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatMenuModule } from '@angular/material/menu';
 import { CoreStore } from '../../../application/core.store';
